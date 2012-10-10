7632b042b2a102d5be5ae563fea2ddf02903a5fe
Rens Admiraal <rens.admiraal@typo3.org>
2012-10-10 09:39:49 +0200

    [TASK] CGL Cleanup

--- Classes/Debug/Toolbar/Debugger/SignalDebugger.php
+++ Classes/Debug/Toolbar/Debugger/SignalDebugger.php
@@ -2,7 +2,7 @@
 namespace Debug\Toolbar\Debugger;
 
 /*                                                                        *
- * This script belongs to the Flow framework.                            *
+ * This script belongs to the FLOW3 package "Debug.Toolbar".              *
  *                                                                        *
  * It is free software; you can redistribute it and/or modify it under    *
  * the terms of the GNU Lesser General Public License, either version 3   *
@@ -17,33 +17,33 @@ use TYPO3\Flow\Annotations as Flow;
  */
 class SignalDebugger {
 
-    /**
-     * @var \TYPO3\Flow\Object\ObjectManagerInterface
-     * @Flow\Inject
-     */
-    protected $objectManager;
+	/**
+	 * @var \TYPO3\Flow\Object\ObjectManagerInterface
+	 * @Flow\Inject
+	 */
+	protected $objectManager;
 
-    /**
-    * TODO: Document this Method!
-    */
-    public function preToolbarRendering() {
-        $dispatcher = $this->objectManager->get('TYPO3\\Flow\\SignalSlot\\Dispatcher');
-        if(method_exists($dispatcher, "getSignals")){
-            $classes = $this->objectManager->get('TYPO3\\Flow\\SignalSlot\\Dispatcher')->getSignals();
-            \Debug\Toolbar\Service\Collector::getModule("Signals")
-                ->getToolbar()
-                    ->addText('Signals')
-                    ->addBadge(count($classes))
-                ->getPopup()
-                    ->addPartial('Signals', array(
-                        'classes' => $classes
-                    ))
-                ->getPanel()
-                    ->addPartial('Signals', array(
-                        'classes' => $classes
-                    ));
-        }
-    }
+	/**
+	 * TODO: Document this Method!
+	 */
+	public function preToolbarRendering() {
+		$dispatcher = $this->objectManager->get('TYPO3\\Flow\\SignalSlot\\Dispatcher');
+		if (method_exists($dispatcher, 'getSignals')) {
+			$classes = $this->objectManager->get('TYPO3\\Flow\\SignalSlot\\Dispatcher')->getSignals();
+			\Debug\Toolbar\Service\Collector::getModule('Signals')
+				->getToolbar()
+				->addText('Signals')
+				->addBadge(count($classes))
+				->getPopup()
+				->addPartial('Signals', array(
+				'classes' => $classes
+			))
+				->getPanel()
+				->addPartial('Signals', array(
+				'classes' => $classes
+			));
+		}
+	}
 
 }
 
--- Classes/Debug/Toolbar/Toolbar/View.php
+++ Classes/Debug/Toolbar/Toolbar/View.php
@@ -2,7 +2,7 @@
 namespace Debug\Toolbar\Toolbar;
 
 /*                                                                        *
- * This script belongs to the Flow framework.                            *
+ * This script belongs to the FLOW3 package "Debug.Toolbar".              *
  *                                                                        *
  * It is free software; you can redistribute it and/or modify it under    *
  * the terms of the GNU Lesser General Public License, either version 3   *
@@ -19,63 +19,61 @@ use TYPO3\Flow\Annotations as Flow;
  */
 class View {
 
-    /**
-     * @var \TYPO3\Fluid\View\StandaloneView
-     */
-    protected $view;
+	/**
+	 * @var \TYPO3\Fluid\View\StandaloneView
+	 */
+	protected $view;
 
-    /**
-     * TODO: Document this Method! ( __construct )
-     */
-    public function __construct() {
-        $this->view = new \TYPO3\Fluid\View\StandaloneView();
-        $this->view->setTemplatePathAndFilename('resource://Debug.Toolbar/Private/Templates/Toolbar.html');
-        $this->view->setPartialRootPath('resource://Debug.Toolbar/Private/Partials/');
-        $this->view->setFormat('html');
-    }
+	/**
+	 * TODO: Document this Method! ( __construct )
+	 */
+	public function __construct() {
+		$this->view = new \TYPO3\Fluid\View\StandaloneView();
+		$this->view->setTemplatePathAndFilename('resource://Debug.Toolbar/Private/Templates/Toolbar.html');
+		$this->view->setPartialRootPath('resource://Debug.Toolbar/Private/Partials/');
+		$this->view->setFormat('html');
+	}
 
-    /**
-     * TODO: Document this Method! ( attachToolbar )
-     */
-    public static function attachToolbar($content) {
-        $toolbar = new \Debug\Toolbar\Toolbar\View();
-        if (stristr($content ,"</body>")) {
-            $content = str_replace('</body>', (('   ' . $toolbar->render()) . PHP_EOL) . '</body>', $content);
-        } else {
-            $content.= $toolbar->render();
-        }
-        return $content;
-    }
+	/**
+	 * TODO: Document this Method! ( attachToolbar )
+	 */
+	public static function attachToolbar($content) {
+		$toolbar = new \Debug\Toolbar\Toolbar\View();
+		if (stristr($content, '</body>')) {
+			$content = str_replace('</body>', (('   ' . $toolbar->render()) . PHP_EOL) . '</body>', $content);
+		} else {
+			$content .= $toolbar->render();
+		}
+		return $content;
+	}
 
-    /**
-     * TODO: Document this Method! ( handleRedirects )
-     */
-    public static function handleRedirects($request, $response) {
-        $previousTokens = array(
+	/**
+	 * TODO: Document this Method! ( handleRedirects )
+	 */
+	public static function handleRedirects($request, $response) {
+		$previousTokens = array();
+		if ($request->hasArgument('__previousDebugToken')) {
+			$previousTokens = explode(',', $request->getArgument('__previousDebugToken'));
+			\Debug\Toolbar\Service\DataStorage::set('Request:RedirectedRequest', implode(',', $previousTokens));
+		}
+		if (intval($response->getStatus()) == 303) {
+			$previousTokens[] = \Debug\Toolbar\Service\DataStorage::get('Environment:Token');
+			$location = $response->getHeaders()->get('Location');
+			$location .= stristr($location, '?') ? '&' : '?';
+			$location .= '__previousDebugToken=' . implode(',', $previousTokens);
+			$response->getHeaders()->set('Location', $location);
+			$response->setContent(('<html><head><meta http-equiv="refresh" content="0;url=' . $location) . '"/></head></html>');
+		}
+	}
 
-        );
-        if ($request->hasArgument('__previousDebugToken')) {
-            $previousTokens = explode(',', $request->getArgument('__previousDebugToken'));
-            \Debug\Toolbar\Service\DataStorage::set('Request:RedirectedRequest', implode(',', $previousTokens));
-        }
-        if (intval($response->getStatus()) == 303) {
-            $previousTokens[] = \Debug\Toolbar\Service\DataStorage::get('Environment:Token');
-            $location = $response->getHeaders()->get('Location');
-            $location .= stristr($location, '?') ? '&' : '?';
-            $location .= '__previousDebugToken=' . implode(',', $previousTokens);
-            $response->getHeaders()->set('Location', $location);
-            $response->setContent(('<html><head><meta http-equiv="refresh" content="0;url=' . $location) . '"/></head></html>');
-        }
-    }
-
-    /**
-     * TODO: Document this Method! ( render )
-     */
-    public function render() {
-        \Debug\Toolbar\Service\DataStorage::save();
-        $this->view->assign('modules', \Debug\Toolbar\Service\Collector::getModules());
-        return $this->view->render();
-    }
+	/**
+	 * TODO: Document this Method! ( render )
+	 */
+	public function render() {
+		\Debug\Toolbar\Service\DataStorage::save();
+		$this->view->assign('modules', \Debug\Toolbar\Service\Collector::getModules());
+		return $this->view->render();
+	}
 
 }
 
